fa604dd190f078439dd9945a29eef03f9e57d7bf
Risqi Bagus <risqibaguspahlevi@gmail.com>
2023-10-26 23:47:11 +0700

    Update Endpoint

--- endpoint.js
+++ endpoint.js
@@ -247,7 +247,7 @@ app.post('/api/v1/transactions', async (req, res) => {
     }
   });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server berjalan di port ${PORT}`);
